5ac0c656d512c2019c25f290b3819eec7f111092
jiangyao <1127056420@qq.com>
2020-07-13 19:58:39 +0800

    'stash'

--- user-srv/handler/user.go
+++ user-srv/handler/user.go
@@ -2,7 +2,7 @@ package handler
 
 import (
 	"context"
-
+	"micro_demo/comm/logging"
 	log "github.com/micro/go-micro/v2/logger"
 	pbUser "micro_demo/proto/user"
 	modelUser "micro_demo/user-srv/model/user"
@@ -10,39 +10,137 @@ import (
 
 type Service struct{}
 
+
 var (
-	userService us.Service
+	userService modelUser.Service
 )
 
 // Init 初始化handler
 func Init() {
 	var err error
-	userService, err = us.GetService()
+	userService, err = modelUser.GetService()
 	if err != nil {
 		log.Fatal("[Init] 初始化Handler错误")
 		return
 	}
 }
 
-// QueryUserByName 通过参数中的名字返回用户
-func (e *Service) QueryUserByName(ctx context.Context, req *pbUser.AddUserReq, rsp *pbUser.AddUserRsp) error {
+// AddUser 添加用户信息
+func (e *Service) AddUser(ctx context.Context, req *pbUser.AddUserReq, rsp *pbUser.AddUserRsp) error {
+	rsp = &pbUser.AddUserRsp{
+		BaseResponse: &pbUser.BaseResponse{
+			Success: true,
+			Error:   nil,
+		},
+	}
+
 	err := userService.AddUser(modelUser.User{
-		
+		Phone:      req.Phone,
+		Nick:       req.Nick,
 	})
-	
 
-	// user, err := userService.QueryUserByName(req.UserName)
-	// if err != nil {
-	// 	rsp.BaseResponse.Success = false
-	// 	rsp.BaseResponse.Error= &s.Error{
-	// 		Code:   500,
-	// 		Message: err.Error(),
-	// 	}
+	if err != nil {
+		logging.Logger().Error(err)
+		rsp.BaseResponse.Success = false
+		rsp.BaseResponse.Error = &pbUser.Error{
+			Code:    500,
+			Message: err.Error(),
+		}
+	}
 
-	// 	return nil
-	// }
+	return nil
+}
 
-	// rsp.User = user
-	// rsp.BaseResponse.Success = true
-	// return nil
+
+// GetFromUid 根据用户id获取信息
+func (e *Service) GetFromUid(ctx context.Context, req *pbUser.GetFromUidReq, rsp *pbUser.GetFromUidRsp) error {
+	rsp = &pbUser.GetFromUidRsp{
+		BaseResponse: &pbUser.BaseResponse{
+			Success: true,
+			Error:   nil,
+		},
+		UserInfo: nil,
+	}
+
+	data,err := userService.GetFromUId(req.Uid)
+	if err != nil {
+		logging.Logger().Error(err)
+		rsp.BaseResponse.Success = false
+		rsp.BaseResponse.Error = &pbUser.Error{
+			Code:    500,
+			Message: err.Error(),
+		}
+	}
+
+	if nil == data{
+		return  nil
+	}
+
+
+	rsp.UserInfo = &pbUser.UserInfo{
+		Uid:   data.UId,
+		Nick:  data.Nick,
+		Phone: data.Phone,
+	}
+
+	return nil
+}
+
+// GetFromPhone 根据手机号获取信息
+func (e *Service) GetFromPhone(ctx context.Context, req *pbUser.GetFromPhoneReq, rsp *pbUser.GetFromPhoneRsp) error {
+	rsp = &pbUser.GetFromPhoneRsp{
+		BaseResponse: &pbUser.BaseResponse{
+			Success: true,
+			Error:   nil,
+		},
+		UserInfo: nil,
+	}
+
+	data,err := userService.GetFromPhone(req.Phone)
+	if err != nil {
+		logging.Logger().Error(err)
+		rsp.BaseResponse.Success = false
+		rsp.BaseResponse.Error = &pbUser.Error{
+			Code:    500,
+			Message: err.Error(),
+		}
+	}
+
+	if nil == data{
+		return  nil
+	}
+
+
+	rsp.UserInfo = &pbUser.UserInfo{
+		Uid:   data.UId,
+		Nick:  data.Nick,
+		Phone: data.Phone,
+	}
+
+	return nil
+}
+
+// UpdateUser 更新用户信息
+func (e *Service) UpdateUser(ctx context.Context, req *pbUser.UpdateUserReq, rsp *pbUser.UpdateUserRsp) error {
+	rsp = &pbUser.UpdateUserRsp{
+		BaseResponse: &pbUser.BaseResponse{
+			Success: true,
+			Error:   nil,
+		},
+	}
+
+	err := userService.AddUser(modelUser.User{
+		Nick:       req.Nick,
+	})
+
+	if err != nil {
+		logging.Logger().Error(err)
+		rsp.BaseResponse.Success = false
+		rsp.BaseResponse.Error = &pbUser.Error{
+			Code:    500,
+			Message: err.Error(),
+		}
+	}
+
+	return nil
 }
