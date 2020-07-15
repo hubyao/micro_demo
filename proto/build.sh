1d552a2cf053efa03baff55690d502d3dfb9e12f
jiangyao <1127056420@qq.com>
2020-07-15 12:43:24 +0800

    fix pb 生成脚本

--- proto/build.sh
+++ proto/build.sh
@@ -4,8 +4,9 @@ for i in ${dir}
 do
 echo -n "{$i}  ->"
 
-# protoc --go_out=. user.proto
 
-protoc --proto_path=${GOPATH}/src:. --micro_out=. --go_out=. $i/*.proto
+#protoc --proto_path=. --micro_out=. --go_out=.  ./greeter/greeter.proto
+#protoc  --micro_out=. --go_out=. $i/*.proto
+protoc --proto_path=. --micro_out=. --go_out=.  ./$i/*.proto
 done
 
