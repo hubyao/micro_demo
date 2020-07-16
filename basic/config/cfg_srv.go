44f34d27cfe542f949b871113c4a61b4d0cf5184
jiangyao <1127056420@qq.com>
2020-07-16 11:38:44 +0800

    v2进行中

---
+++ basic/config/cfg_srv.go
@@ -0,0 +1,152 @@
+package config
+
+import (
+	"crypto/md5"
+	"flag"
+	"fmt"
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/config/source/file"
+	log "github.com/micro/go-micro/v2/logger"
+	proto "github.com/micro/go-plugins/config/source/grpc/v2/proto"
+	"micro_demo/basic/common"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	mux        sync.RWMutex
+	configMaps = make(map[string]*proto.ChangeSet)
+	// 根据需要改成可配置的app列表
+	apps = []string{"micro"}
+)
+
+var (
+	env string  // 环境
+)
+
+
+func WatchCfg(){
+
+	// 解析环境
+	flagParseEnv()
+	// 灾难恢复
+	defer func() {
+		if r := recover(); r != nil {
+			log.Infof("[main] Recovered in f %v", r)
+		}
+	}()
+
+	// 加载并侦听配置文件
+	err := loadAndWatchConfigFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	read()
+}
+
+
+// Read 读取文件
+func read ()(rsp *proto.ReadResponse, err error) {
+	appName := parsePath("micro")
+
+	rsp = &proto.ReadResponse{
+		ChangeSet: getConfig(appName),
+	}
+
+	return
+}
+
+
+
+
+
+// getConfig 获取文件
+func getConfig(appName string) *proto.ChangeSet {
+	bytes := config.Get(appName).Bytes()
+	return &proto.ChangeSet{
+		Data:      bytes,
+		Checksum:  fmt.Sprintf("%x", md5.Sum(bytes)),
+		Format:    "yml",
+		Source:    "file",
+		Timestamp: time.Now().Unix()}
+}
+
+
+// parsePath 解析地址
+func parsePath(path string) (appName string) {
+	paths := strings.Split(path, "/")
+
+	if paths[0] == "" && len(paths) > 1 {
+		return paths[1]
+	}
+
+	return paths[0]
+}
+
+
+
+
+// loadAndWatchConfigFile 加载文件
+func loadAndWatchConfigFile() (err error) {
+	// 加载每个应用的配置文件
+	for _, app := range apps {
+		if err := config.Load(file.NewSource(
+			file.WithPath("./conf/" + app + ".yml"),
+		)); err != nil {
+			log.Fatalf("[loadAndWatchConfigFile] 加载应用配置文件 异常，%s", err)
+			return err
+		}
+	}
+
+	// 侦听文件变动
+	watcher, err := config.Watch()
+	if err != nil {
+		log.Fatalf("[loadAndWatchConfigFile] 开始侦听应用配置文件变动 异常，%s", err)
+		return err
+	}
+
+	go func() {
+		for {
+			v, err := watcher.Next()
+			if err != nil {
+				log.Fatalf("[loadAndWatchConfigFile] 侦听应用配置文件变动 异常， %s", err)
+				return
+			}
+
+			log.Infof("[loadAndWatchConfigFile] 文件变动，%s", string(v.Bytes()))
+		}
+	}()
+
+	return
+}
+
+
+// flagParse 参数解析
+func flagParseEnv(){
+	// 获取命令行参数
+	flag.StringVar(&env, "env", "", "环境, 可选项 loc,dev,prod")
+	flag.Parse()
+
+	if env == ""{
+		env = common.EnvLoc
+	}
+
+	switch env {
+	case common.EnvLoc:
+		apps = []string{"micro_loc"}
+	case common.EnvDev:
+		apps = []string{"micro_dev"}
+	case common.EnvProd:
+		apps = []string{"micro_prod"}
+	default:
+		fmt.Println("env只支持 loc,dev,prod")
+		os.Exit(1)
+	}
+
+	log.Infof("启动环境: %v",env)
+
+}
